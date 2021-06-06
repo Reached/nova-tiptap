afa52425a6d876ffa8500bc4aa919fad0af2aaa3
Casper Sørensen <casper.aarby.sorensen@gmail.com>
2021-06-06 18:44:03 +0200

    WIP

--- resources/js/field.js
+++
@@ -1,6 +0,0 @@
-window.pretty = require('pretty');
-
-Nova.booting((Vue, router) => {
-    Vue.component('detail-tiptap', require('./components/DetailField.vue').default);
-    Vue.component('form-tiptap', require('./components/FormField.vue').default);
-})
